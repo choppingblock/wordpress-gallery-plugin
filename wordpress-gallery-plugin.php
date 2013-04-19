fd3c831080c1db31615d83ae0a392e1f81538f9e
Matthew Richmond <matthew@joshua.svacacave.net>
2013-04-18 21:52:43 -0400

    Initial plugin file created

---
+++ wordpress-gallery-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Choppingblocks Wordpress Gallery Plugin
+Plugin URI: https://github.com/choppingblock/wordpress-gallery-plugin
+Description: a sample plugin to show how it is done
+Version: 0.2
+Author: Matthew Richmond
+Author URI: http://choppingblock.com
+License: GPL2
+*/
+
+?>
